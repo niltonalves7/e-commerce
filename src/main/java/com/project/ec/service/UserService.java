aa92a1e5a87a8c6a57e063584bb54f9628ff605e
Nilton <nilttonalvez@gmail.com>
2026-02-27 00:15:14 -0300

    implementing services

--- src/main/java/com/project/ec/service/UserService.java
+++ src/main/java/com/project/ec/service/UserService.java
@@ -1,4 +1,33 @@
 package com.project.ec.service;
 
+import com.project.ec.entity.User;
+import com.project.ec.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User createNewUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User findById(UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+    }
+
 }
