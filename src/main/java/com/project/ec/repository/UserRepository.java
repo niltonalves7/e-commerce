bbc978ad7ebe630be88b343413f03687edbc4738
Nilton <nilttonalvez@gmail.com>
2026-02-27 00:14:55 -0300

    creating repository

--- src/main/java/com/project/ec/repository/UserRepository.java
+++ src/main/java/com/project/ec/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.project.ec.repository;
 
-public interface UserRepository {
+import com.project.ec.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
