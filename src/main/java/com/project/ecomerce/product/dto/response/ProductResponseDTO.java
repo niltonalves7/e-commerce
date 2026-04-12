efd76a1e6749597eca21a32702a40147ce80baab
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:03:20 -0300

    creating product response dto

--- src/main/java/com/project/ecomerce/product/dto/response/ProductResponseDTO.java
+++ src/main/java/com/project/ecomerce/product/dto/response/ProductResponseDTO.java
@@ -1,4 +1,19 @@
 package com.project.ecomerce.product.dto.response;
 
-public record ProductResponseDTO() {
-}
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ProductResponseDTO(
+
+        UUID id,
+        String name,
+        String description,
+        BigDecimal price,
+        Integer stock,
+        String imageUrl,
+
+        LocalDateTime createdAt
+) {}
