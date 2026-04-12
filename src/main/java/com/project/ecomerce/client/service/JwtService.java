96f6b772f92668485f53892bfd7786628c72a83e
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:32:13 -0300

    implementing jwt service

--- src/main/java/com/project/ecomerce/client/service/JwtService.java
+++ src/main/java/com/project/ecomerce/client/service/JwtService.java
@@ -1,16 +1,44 @@
-package com.project.ecomerce.security;
+package com.project.ecomerce.client.service;
 
 import com.project.ecomerce.client.entity.Client;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "secret-key-super-safe"; // depois vamos melhorar isso
+    @Value("${JWT_SECRET}")
+    private String secretKey;
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractUsername(token);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 
     public String generateToken(Client client) {
 
@@ -19,7 +47,11 @@ public class JwtService {
                 .claim("role", client.getRole().name())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
+    }
+
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 }
