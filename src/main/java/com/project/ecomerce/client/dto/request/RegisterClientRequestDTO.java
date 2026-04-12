c473a767d60a80e4bcacc1780a77be362628330d
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:38:09 -0300

    creating reqiest dto of register

--- src/main/java/com/project/ecomerce/client/dto/request/RegisterClientRequestDTO.java
+++ src/main/java/com/project/ecomerce/client/dto/request/RegisterClientRequestDTO.java
@@ -2,7 +2,7 @@ package com.project.ecomerce.client.dto.request;
 
 import jakarta.validation.constraints.*;
 
-public record CreateClientRequestDTO(
+public record RegisterClientRequestDTO(
         @NotBlank (message = "Name is required") String name,
         @Email @NotBlank (message = "Invalid Email")String email,
         @NotBlank (message = "Password is required")String password
