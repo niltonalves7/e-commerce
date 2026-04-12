e325c4544cccbfe4e720a91a9d8370fdfaf43f39
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:02:13 -0300

    mapping product

--- src/main/java/com/project/ecomerce/product/mapper/ProductMapper.java
+++ src/main/java/com/project/ecomerce/product/mapper/ProductMapper.java
@@ -1,4 +1,14 @@
 package com.project.ecomerce.product.mapper;
 
-public class ProductMapper {
+import com.project.ecomerce.product.dto.request.CreateProductRequestDTO;
+import com.project.ecomerce.product.dto.response.ProductResponseDTO;
+import com.project.ecomerce.product.entity.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product toEntity(CreateProductRequestDTO request);
+
+    ProductResponseDTO toResponse(Product product);
 }
