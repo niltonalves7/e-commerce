f8fbec663ac8fbba86417c933af39efc84b20a29
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:37:28 -0300

    creating response dto for client login

--- src/main/java/com/project/ecomerce/client/dto/response/LoginClientResponseDTO.java
+++ src/main/java/com/project/ecomerce/client/dto/response/LoginClientResponseDTO.java
@@ -1,4 +1,12 @@
 package com.project.ecomerce.client.dto.response;
 
-public record ClientResponse() {
-}
+import com.project.ecomerce.common.enums.Roles;
+
+public record LoginClientResponseDTO(
+
+    String token,
+    String tokenType,  // ex: "Bearer"
+    String name,
+    String email,
+    Roles role
+){}
