0f6573784068bc9b4d3e9af0b45a5c1f02e4de76
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:22:10 -0300

    mapping order

--- src/main/java/com/project/ecomerce/order/mapper/OrderMapper.java
+++ src/main/java/com/project/ecomerce/order/mapper/OrderMapper.java
@@ -1,4 +1,21 @@
 package com.project.ecomerce.order.mapper;
 
+import com.project.ecomerce.order.dto.response.OrderItemResponseDTO;
+import com.project.ecomerce.order.dto.response.OrderResponseDTO;
+import com.project.ecomerce.order.entity.Order;
+import com.project.ecomerce.order.entity.OrderItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
+
+    @Mapping(source = "client.id", target = "clientId")
+    @Mapping(source = "items", target = "items")
+    OrderResponseDTO toResponse(Order order);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    OrderItemResponseDTO toItemResponse(OrderItem item);
 }
