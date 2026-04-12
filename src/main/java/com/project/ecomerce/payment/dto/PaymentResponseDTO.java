1c1761f70bc4a1bfadc6c06e5f552821d77916ef
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:13:22 -0300

    creating response dto for payment

--- src/main/java/com/project/ecomerce/payment/dto/PaymentResponseDTO.java
+++ src/main/java/com/project/ecomerce/payment/dto/PaymentResponseDTO.java
@@ -1,4 +1,18 @@
 package com.project.ecomerce.payment.dto;
 
-public record PaymentResponseDTO() {
-}
+import com.project.ecomerce.common.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record PaymentResponseDTO(
+
+        UUID id,
+        UUID orderId,
+        BigDecimal amount,
+        PaymentStatus status,
+        String stripePaymentIntentId,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {}
