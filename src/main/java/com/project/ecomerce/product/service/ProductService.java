724878c1d040327f9dea79697df5a61255eff695
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:01:15 -0300

    creating interface for product service

--- src/main/java/com/project/ecomerce/product/service/ProductService.java
+++ src/main/java/com/project/ecomerce/product/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.project.ecomerce.product.service;
 
+import com.project.ecomerce.product.dto.request.CreateProductRequestDTO;
+import com.project.ecomerce.product.dto.response.ProductResponseDTO;
+import java.util.List;
+import java.util.UUID;
+
 public interface ProductService {
+    ProductResponseDTO createProduct(CreateProductRequestDTO productDto);
+    ProductResponseDTO getProductById(UUID id);
+    List<ProductResponseDTO> getAllProducts();
+    ProductResponseDTO updateProduct(UUID id, CreateProductRequestDTO productDto);
+    void deleteProduct(UUID id);
 }
