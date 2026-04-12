c4a8989df5566c03bc3bbb9af374a6bd83361973
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:31:31 -0300

    implementing payment status

--- src/main/java/com/project/ecomerce/common/enums/PaymentStatus.java
+++ src/main/java/com/project/ecomerce/common/enums/PaymentStatus.java
@@ -1,4 +1,30 @@
 package com.project.ecomerce.common.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum PaymentStatus {
+
+    PENDING("pending"),             // aguardando pagamento
+    PROCESSING("processing"),       // pagamento em processamento
+    PAID("paid"),         // pagamento concluído
+    FAILED("failed"),               // pagamento falhou
+    CANCELED("canceled"),           // cancelado
+    REFUNDED("refunded");           // reembolsado
+
+    private final String value;
+
+    PaymentStatus(String value) {
+        this.value = value;
+    }
+
+    // 🔹 Conversão segura de String → Enum
+    public static PaymentStatus fromValue(String value) {
+        for (PaymentStatus status : PaymentStatus.values()) {
+            if (status.value.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid payment status: " + value);
+    }
 }
