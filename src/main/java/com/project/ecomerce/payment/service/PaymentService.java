5965489cb97442ed262701e5e355f4beeadbe23a
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:09:22 -0300

    creating interface of payment service

--- src/main/java/com/project/ecomerce/payment/service/PaymentService.java
+++ src/main/java/com/project/ecomerce/payment/service/PaymentService.java
@@ -1,4 +1,12 @@
 package com.project.ecomerce.payment.service;
 
+import com.project.ecomerce.payment.entity.Payment;
+
+import java.util.UUID;
+
 public interface PaymentService {
+
+    Payment createPayment(UUID orderId);
+    Payment getPaymentByOrderId(UUID orderId);
+    void deletePaymentByOrderId(UUID orderId);
 }
