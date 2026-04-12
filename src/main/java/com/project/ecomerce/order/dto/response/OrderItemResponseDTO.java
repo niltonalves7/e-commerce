73e34b0f03cb7428f14a7e7f0066eab621179765
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:25:13 -0300

    creating response dto for order item

--- src/main/java/com/project/ecomerce/order/dto/response/OrderItemResponseDTO.java
+++ src/main/java/com/project/ecomerce/order/dto/response/OrderItemResponseDTO.java
@@ -1,4 +1,12 @@
 package com.project.ecomerce.order.dto.response;
 
-public record OrderItemResponseDTO() {
-}
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record OrderItemResponseDTO(
+        UUID id,
+        UUID productId,
+        String productName,
+        Integer quantity,
+        BigDecimal unitPrice
+) {}
