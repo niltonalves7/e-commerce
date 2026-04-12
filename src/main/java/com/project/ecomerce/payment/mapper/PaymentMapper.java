d1020f78d09236ffbef70e3be294689eed49b49b
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:12:18 -0300

    mapping entity payment

--- src/main/java/com/project/ecomerce/payment/mapper/PaymentMapper.java
+++ src/main/java/com/project/ecomerce/payment/mapper/PaymentMapper.java
@@ -1,4 +1,13 @@
 package com.project.ecomerce.payment.mapper;
 
-public class PaymentMapper {
+import com.project.ecomerce.payment.dto.PaymentResponseDTO;
+import com.project.ecomerce.payment.entity.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+
+    @Mapping(source = "order.id", target = "orderId")
+    PaymentResponseDTO toResponse(Payment payment);
 }
