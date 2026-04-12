204af4fd4053b2f11845bf6eb0278d947d81e6c7
Nilton <nilttonalvez@gmail.com>
2026-04-12 01:57:37 -0300

    implementing userdetails for authentication

--- src/main/java/com/project/ecomerce/security/service/CustomUserDetailsService.java
+++ src/main/java/com/project/ecomerce/security/service/CustomUserDetailsService.java
@@ -1,4 +1,32 @@
 package com.project.ecomerce.security.service;
 
-public class CustomUserDetailsService {
+import com.project.ecomerce.client.entity.Client;
+import com.project.ecomerce.client.repository.ClientRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final ClientRepository clientRepository;
+
+    public CustomUserDetailsService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Client client = clientRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(
+                client.getEmail(),
+                client.getPassword(),
+                List.of(new SimpleGrantedAuthority(client.getRole().getRole()))
+        );
+    }
 }
