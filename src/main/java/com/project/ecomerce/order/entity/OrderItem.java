704f592466cc4a7b775b3cc862fefa267c45c9c4
Nilton <nilttonalvez@gmail.com>
2026-04-12 02:22:27 -0300

    mapping order item

--- src/main/java/com/project/ecomerce/order/entity/OrderItem.java
+++ src/main/java/com/project/ecomerce/order/entity/OrderItem.java
@@ -1,4 +1,37 @@
 package com.project.ecomerce.order.entity;
 
+import com.project.ecomerce.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderItem {
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Id private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
 }
