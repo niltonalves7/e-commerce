20bbce13dadd471c170ca67653df08091230532a
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:17:09 -0300

    feat: implementing pagination

--- src/main/java/com/project/ecommerce/domain/product/service/ProductService.java
+++ src/main/java/com/project/ecommerce/domain/product/service/ProductService.java
@@ -1,70 +1,79 @@
 package com.project.ecommerce.domain.product.service;
 
-import com.project.ecommerce.infra.exception.ResourceNotFoundException;
+import com.project.ecommerce.domain.category.entity.Category;
+import com.project.ecommerce.domain.category.repository.CategoryRepository;
 import com.project.ecommerce.domain.product.dto.request.CreateProductRequestDTO;
+import com.project.ecommerce.domain.product.dto.request.UpdateProductRequestDTO;
 import com.project.ecommerce.domain.product.dto.response.ProductResponseDTO;
 import com.project.ecommerce.domain.product.entity.Product;
-import com.project.ecommerce.domain.product.repository.ProductRepository;
-import org.springframework.stereotype.Service;
 import com.project.ecommerce.domain.product.mapper.ProductMapper;
+import com.project.ecommerce.domain.product.repository.ProductRepository;
+import com.project.ecommerce.infra.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
-public class ProductServiceImpl implements ProductService {
+@RequiredArgsConstructor
+public class ProductService {
 
     private final ProductRepository productRepository;
-    private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
+    private final ProductMapper mapper;
 
-    public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper) {
-        this.productRepository = productRepository;
-        this.productMapper = productMapper;
+    public ProductResponseDTO createProduct(CreateProductRequestDTO request) {
+        Category category = findCategoryById(request.categoryId());
+
+        Product product = mapper.toEntity(request);
+        product.setCategory(category);
+
+        return mapper.toResponse(productRepository.save(product));
     }
 
-    @Override
-    public ProductResponseDTO createProduct(CreateProductRequestDTO productDto) {
-        Product product = productMapper.toEntity(productDto);
-        Product saved = productRepository.save(product);
-        return productMapper.toResponse(saved);
+    public Page<ProductResponseDTO> getAllProducts(UUID categoryId, String name, Pageable pageable) {
+        if (categoryId != null && name != null) {
+            return productRepository.findByCategoryIdAndNameContainingIgnoreCase(categoryId, name, pageable)
+                    .map(mapper::toResponse);
+        } else if (categoryId != null) {
+            return productRepository.findByCategoryId(categoryId, pageable)
+                    .map(mapper::toResponse);
+        } else if (name != null) {
+            return productRepository.findByNameContainingIgnoreCase(name, pageable)
+                    .map(mapper::toResponse);
+        }
+        return productRepository.findAll(pageable)
+                .map(mapper::toResponse);
     }
 
-    @Override
     public ProductResponseDTO getProductById(UUID id) {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Product not found!"));
-        return productMapper.toResponse(product);
+        return mapper.toResponse(findProductById(id));
     }
 
-    @Override
-    public List<ProductResponseDTO> getAllProducts() {
-        return productRepository.findAll()
-                .stream()
-                .map(productMapper::toResponse)
-                .toList();
+    public ProductResponseDTO updateProduct(UUID id, UpdateProductRequestDTO request) {
+        Product product = findProductById(id);
+
+        if (request.categoryId() != null) {
+            product.setCategory(findCategoryById(request.categoryId()));
+        }
+
+        mapper.updateEntityFromDTO(request, product);
+        return mapper.toResponse(productRepository.save(product));
     }
 
-    @Override
-    public ProductResponseDTO updateProduct(UUID id, CreateProductRequestDTO productDto) {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Product not found!"));
-
-        product.setName(productDto.name());
-        product.setPrice(productDto.price());
-        product.setDescription(productDto.description());
-        product.setStock(productDto.stock());
-        product.setImageUrl(productDto.imageUrl());
-
-        Product updated = productRepository.save(product);
-        return productMapper.toResponse(updated);
-    }
-
-    @Override
     public void deleteProduct(UUID id) {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Product not found"));
+        productRepository.delete(findProductById(id));
+    }
 
-        productRepository.delete(product);
+    private Product findProductById(UUID id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    private Category findCategoryById(UUID id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
     }
 }
