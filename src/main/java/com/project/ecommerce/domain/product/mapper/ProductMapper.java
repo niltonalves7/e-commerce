e21d7ccbd9642d32acde439c660334e502e4051f
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:17:42 -0300

    refactor: small adjustments

--- src/main/java/com/project/ecommerce/domain/product/mapper/ProductMapper.java
+++ src/main/java/com/project/ecommerce/domain/product/mapper/ProductMapper.java
@@ -1,14 +1,28 @@
-package com.project.ecomerce.domain.product.mapper;
+package com.project.ecommerce.domain.product.mapper;
 
-import com.project.ecomerce.domain.product.dto.request.CreateProductRequestDTO;
-import com.project.ecomerce.domain.product.dto.response.ProductResponseDTO;
-import com.project.ecomerce.domain.product.entity.Product;
+import com.project.ecommerce.domain.category.mapper.CategoryMapper;
+import com.project.ecommerce.domain.product.dto.request.CreateProductRequestDTO;
+import com.project.ecommerce.domain.product.dto.request.UpdateProductRequestDTO;
+import com.project.ecommerce.domain.product.dto.response.ProductResponseDTO;
+import com.project.ecommerce.domain.product.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Product toEntity(CreateProductRequestDTO request);
 
     ProductResponseDTO toResponse(Product product);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromDTO(UpdateProductRequestDTO request, @MappingTarget Product product);
 }
