34d07f2bbee57e0c94fba93361fdf5034f261b0d
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:20:16 -0300

    refactor: changind endpoints according to the service

--- src/main/java/com/project/ecommerce/domain/product/controller/ProductController.java
+++ src/main/java/com/project/ecommerce/domain/product/controller/ProductController.java
@@ -1,32 +1,41 @@
-package com.project.ecomerce.domain.product.controller;
+package com.project.ecommerce.domain.product.controller;
 
-import com.project.ecomerce.domain.product.dto.request.CreateProductRequestDTO;
-import com.project.ecomerce.domain.product.dto.response.ProductResponseDTO;
-import com.project.ecomerce.domain.product.service.ProductService;
+import com.project.ecommerce.domain.product.dto.request.CreateProductRequestDTO;
+import com.project.ecommerce.domain.product.dto.request.UpdateProductRequestDTO;
+import com.project.ecommerce.domain.product.dto.response.ProductResponseDTO;
+import com.project.ecommerce.domain.product.service.ProductService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) { this.productService = productService; }
-
     @PostMapping
-    public ResponseEntity<ProductResponseDTO> createProduct( @Valid @RequestBody CreateProductRequestDTO productDto ) {
-
-        ProductResponseDTO response = productService.createProduct(productDto);
+    public ResponseEntity<ProductResponseDTO> createProduct(
+            @RequestBody @Valid CreateProductRequestDTO request) {
+        ProductResponseDTO response = productService.createProduct(request);
         return ResponseEntity
                 .created(URI.create("/products/" + response.id()))
                 .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponseDTO>> getAllProducts(
+            @RequestParam(required = false) UUID categoryId,
+            @RequestParam(required = false) String name,
+            Pageable pageable) {
+        return ResponseEntity.ok(productService.getAllProducts(categoryId, name, pageable));
     }
 
     @GetMapping("/{id}")
@@ -34,14 +43,11 @@ public class ProductController {
         return ResponseEntity.ok(productService.getProductById(id));
     }
 
-    @GetMapping()
-    public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
-        return ResponseEntity.ok(productService.getAllProducts());
-    }
-
     @PutMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> updateProduct( @PathVariable UUID id, @Valid @RequestBody CreateProductRequestDTO productDto ) {
-        return ResponseEntity.ok(productService.updateProduct(id, productDto));
+    public ResponseEntity<ProductResponseDTO> updateProduct(
+            @PathVariable UUID id,
+            @RequestBody @Valid UpdateProductRequestDTO request) {
+        return ResponseEntity.ok(productService.updateProduct(id, request));
     }
 
     @DeleteMapping("/{id}")
