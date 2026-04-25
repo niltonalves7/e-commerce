00396f56b0bea092aa0cf80cdd2156b3c5069bbf
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:19:33 -0300

    refactor: small adjustments

--- src/main/java/com/project/ecommerce/domain/product/dto/request/CreateProductRequestDTO.java
+++ src/main/java/com/project/ecommerce/domain/product/dto/request/CreateProductRequestDTO.java
@@ -1,15 +1,17 @@
-package com.project.ecomerce.domain.product.dto.request;
+package com.project.ecommerce.domain.product.dto.request;
 
 import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record CreateProductRequestDTO(
 
         @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must be at most 100 characters")
         String name,
 
         @NotBlank(message = "Description is required")
-        @Size(max = 500)
+        @Size(max = 500, message = "Description must be at most 500 characters")
         String description,
 
         @NotNull(message = "Price is required")
@@ -18,7 +20,10 @@ public record CreateProductRequestDTO(
 
         @NotNull(message = "Stock is required")
         @PositiveOrZero(message = "Stock cannot be negative")
-        Integer stock,
+        Integer stockQuantity,
 
-        String imageUrl
+        String imageUrl,
+
+        @NotNull(message = "Category is required")
+        UUID categoryId
 ) {}
