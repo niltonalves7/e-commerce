2eba670af259584a752f30e7e4a6236d57409fd4
Nilton <nilttonalvez@gmail.com>
2026-04-25 20:04:49 -0300

    feat: implementing response for cart

--- src/main/java/com/project/ecommerce/domain/cart/dto/response/CartResponseDTO.java
+++ src/main/java/com/project/ecommerce/domain/cart/dto/response/CartResponseDTO.java
@@ -1,4 +1,13 @@
 package com.project.ecommerce.domain.cart.dto.response;
 
-public record CartResponseDTO() {
-}
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record CartResponseDTO(
+        UUID id,
+        List<CartItemResponseDTO> items,
+        BigDecimal total,
+        LocalDateTime updatedAt
+) {}
