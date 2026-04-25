706bac2da2fd0275a9b2af5327d5d3d6761b8983
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:25:14 -0300

    small: adjustments

--- src/main/java/com/project/ecommerce/domain/order/repository/OrderRepository.java
+++ src/main/java/com/project/ecommerce/domain/order/repository/OrderRepository.java
@@ -1,11 +1,12 @@
-package com.project.ecomerce.domain.order.repository;
+package com.project.ecommerce.domain.order.repository;
 
-import com.project.ecomerce.domain.order.entity.Order;
+import com.project.ecommerce.domain.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    Page<Order> findByUserId(UUID userId, Pageable pageable);
 }
