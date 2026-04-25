48f53dfe0e8b8278d3e6688a8136a87bac3a6189
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:15:27 -0300

    refactor: small adjustments

--- src/main/java/com/project/ecommerce/domain/user/dto/request/CreateUserRequestDTO.java
+++ src/main/java/com/project/ecommerce/domain/user/dto/request/CreateUserRequestDTO.java
@@ -1,11 +1,24 @@
 package com.project.ecommerce.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import com.project.ecommerce.shared.enums.Role;
-import jakarta.validation.constraints.*;
 
-public record RegisterUserRequestDTO(
-        @NotBlank (message = "Name is required") String name,
-        @Email @NotBlank (message = "Invalid Email")String email,
-        @NotBlank (message = "Password is required")String password,
+public record CreateUserRequestDTO(
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must be at most 100 characters")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        @Size(max = 150, message = "Email must be at most 150 characters")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password,
+
         Role role
 ) {}
