7b596b0fb61bff1e5ad2f06caeee22634e8f328b
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:28:19 -0300

    refactor: changind endpoints according to the service

--- src/main/java/com/project/ecommerce/domain/order/controller/OrderController.java
+++ src/main/java/com/project/ecommerce/domain/order/controller/OrderController.java
@@ -1,55 +1,48 @@
-package com.project.ecomerce.domain.order.controller;
+package com.project.ecommerce.domain.order.controller;
 
-import com.project.ecomerce.domain.order.dto.request.AddItemRequestDTO;
-import com.project.ecomerce.domain.order.dto.request.CreateOrderRequestDTO;
-import com.project.ecomerce.domain.order.dto.response.OrderResponseDTO;
-import com.project.ecomerce.domain.order.service.OrderService;
+import com.project.ecommerce.domain.order.dto.request.UpdateOrderStatusRequestDTO;
+import com.project.ecommerce.domain.order.dto.response.OrderResponseDTO;
+import com.project.ecommerce.domain.order.service.OrderService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
     private final OrderService orderService;
 
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
-
-    @PostMapping
-    public ResponseEntity<OrderResponseDTO> createOrder( @RequestBody @Valid CreateOrderRequestDTO orderRequest ) {
-        OrderResponseDTO response = orderService.createOrder(orderRequest);
-
+    @PostMapping("/checkout")
+    public ResponseEntity<OrderResponseDTO> checkout() {
+        OrderResponseDTO response = orderService.checkout();
         return ResponseEntity
                 .created(URI.create("/orders/" + response.id()))
                 .body(response);
     }
 
-    @PostMapping("/{orderId}/items")
-    public ResponseEntity<OrderResponseDTO> addItem( @PathVariable UUID orderId, @RequestBody @Valid AddItemRequestDTO itemRequest ) {
-        return ResponseEntity.ok(orderService.addItem(orderId, itemRequest));
-    }
-
     @GetMapping("/{orderId}")
-    public ResponseEntity<OrderResponseDTO> getOrderById( @PathVariable UUID orderId ) {
+    public ResponseEntity<OrderResponseDTO> getOrderById(@PathVariable UUID orderId) {
         return ResponseEntity.ok(orderService.getOrderById(orderId));
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderResponseDTO>> getAllOrders() {
-        return ResponseEntity.ok(orderService.getAllOrders());
+    public ResponseEntity<Page<OrderResponseDTO>> getAllOrders(Pageable pageable) {
+        return ResponseEntity.ok(orderService.getAllOrders(pageable));
     }
 
-    @DeleteMapping("/{orderId}/items/{orderItemId}")
-    public ResponseEntity<?> removeItem( @PathVariable UUID orderId, @PathVariable UUID orderItemId ) {
-        orderService.removeItem(orderId, orderItemId);
+    @PatchMapping("/{orderId}/status")
+    public ResponseEntity<Void> updateStatus(
+            @PathVariable UUID orderId,
+            @RequestBody @Valid UpdateOrderStatusRequestDTO request) {
+        orderService.updateOrderStatus(orderId, request.status());
         return ResponseEntity.noContent().build();
     }
 }
