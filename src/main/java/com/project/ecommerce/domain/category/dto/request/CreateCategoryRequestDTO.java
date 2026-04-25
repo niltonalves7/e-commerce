55bed0be23532ee60bb3a5a3de15f59f5c957f67
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:31:32 -0300

    feat: implementing request for category

--- src/main/java/com/project/ecommerce/domain/category/dto/request/CreateCategoryRequestDTO.java
+++ src/main/java/com/project/ecommerce/domain/category/dto/request/CreateCategoryRequestDTO.java
@@ -1,4 +1,14 @@
 package com.project.ecommerce.domain.category.dto.request;
 
-public class CreateCategoryRequestDTO {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateCategoryRequestDTO(
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must be at most 100 characters")
+        String name,
+
+        @Size(max = 255, message = "Description must be at most 255 characters")
+        String description
+) {}
