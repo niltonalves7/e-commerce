94fd9b4fe0949ca72b484b87bca1d6aa25938bec
Nilton <nilttonalvez@gmail.com>
2026-04-25 20:15:21 -0300

    refactor

--- src/main/java/com/project/ecommerce/EcomerceApplication.java
+++ src/main/java/com/project/ecommerce/EcomerceApplication.java
@@ -1,4 +1,4 @@
-package com.project.ecomerce;
+package com.project.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
