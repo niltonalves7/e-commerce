8b40fbcd66e8c4218a20dacfff7e567d9c166c4e
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:07:55 -0300

    refactor: standardizing reply message

--- src/main/java/com/project/ecommerce/infra/exception/ResourceNotFoundException.java
+++ src/main/java/com/project/ecommerce/infra/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.project.ecomerce.infra.exception;
+package com.project.ecommerce.infra.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
