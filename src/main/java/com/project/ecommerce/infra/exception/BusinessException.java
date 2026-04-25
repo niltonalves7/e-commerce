aaacde26f5d0e725e1036b5b006a242b3ac59e75
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:08:16 -0300

    refactor: standardizing reply message

--- src/main/java/com/project/ecommerce/infra/exception/BusinessException.java
+++ src/main/java/com/project/ecommerce/infra/exception/BusinessException.java
@@ -1,4 +1,4 @@
-package com.project.ecomerce.infra.exception;
+package com.project.ecommerce.infra.exception;
 
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
