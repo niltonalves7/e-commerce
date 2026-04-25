4191151a0b05e73f0657c1af49f73d80ba5ce5c1
Nilton <nilttonalvez@gmail.com>
2026-04-25 20:06:29 -0300

    feat: implementing add request for cartItem

--- src/main/java/com/project/ecommerce/domain/cart/dto/request/AddCartItemRequestDTO.java
+++ src/main/java/com/project/ecommerce/domain/cart/dto/request/AddCartItemRequestDTO.java
@@ -1,4 +1,16 @@
 package com.project.ecommerce.domain.cart.dto.request;
 
-public record AddCartItemRequestDTO() {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.util.UUID;
+
+public record AddCartItemRequestDTO(
+
+        @NotNull(message = "Product is required")
+        UUID productId,
+
+        @NotNull(message = "Quantity is required")
+        @Positive(message = "Quantity must be greater than zero")
+        Integer quantity
+) {}
