a804adc9a43a567966ff633d7c76c006691f82a7
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:32:59 -0300

    feat: implementing repository for cart

--- src/main/java/com/project/ecommerce/domain/cart/repository/CartRepository.java
+++ src/main/java/com/project/ecommerce/domain/cart/repository/CartRepository.java
@@ -1,4 +1,11 @@
 package com.project.ecommerce.domain.cart.repository;
 
-public interface CartRepository {
+import com.project.ecommerce.domain.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findByUserId(UUID userId);
 }
