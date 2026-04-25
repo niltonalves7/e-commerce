467ceb2e2c9700bfc8e508d6028c161b8f4c8917
Nilton <nilttonalvez@gmail.com>
2026-04-25 20:09:34 -0300

    feat: implementing endpoint for register

--- src/main/java/com/project/ecommerce/domain/auth/controller/AuthController.java
+++ src/main/java/com/project/ecommerce/domain/auth/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.project.ecommerce.domain.auth.controller;
 
+import com.project.ecommerce.domain.auth.dto.request.LoginRequestDTO;
+import com.project.ecommerce.domain.auth.dto.response.AuthResponseDTO;
+import com.project.ecommerce.domain.auth.service.AuthService;
+import com.project.ecommerce.domain.user.dto.request.CreateUserRequestDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(@RequestBody @Valid CreateUserRequestDTO request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid LoginRequestDTO request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
 }
