c2f8e9b90e8306f79af4bb4da3a0c5061d60032e
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:21:59 -0300

    refactor: small adjustments

--- src/main/java/com/project/ecommerce/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/project/ecommerce/domain/payment/repository/PaymentRepository.java
@@ -1,15 +1,12 @@
-package com.project.ecomerce.domain.payment.repository;
+package com.project.ecommerce.domain.payment.repository;
 
-import com.project.ecomerce.domain.payment.entity.Payment;
+import com.project.ecommerce.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
-
     Optional<Payment> findByOrderId(UUID orderId);
     Optional<Payment> findByStripePaymentIntentId(String stripePaymentIntentId);
 }
