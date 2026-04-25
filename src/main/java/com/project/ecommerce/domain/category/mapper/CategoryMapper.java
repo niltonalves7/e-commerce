e6423442c2189c1d4b979e4d962500c3deadd448
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:29:24 -0300

    feat: implementing category mapper

--- src/main/java/com/project/ecommerce/domain/category/mapper/CategoryMapper.java
+++ src/main/java/com/project/ecommerce/domain/category/mapper/CategoryMapper.java
@@ -1,4 +1,18 @@
 package com.project.ecommerce.domain.category.mapper;
 
+import com.project.ecommerce.domain.category.dto.request.CreateCategoryRequestDTO;
+import com.project.ecommerce.domain.category.dto.request.UpdateCategoryRequestDTO;
+import com.project.ecommerce.domain.category.dto.response.CategoryResponseDTO;
+import com.project.ecommerce.domain.category.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
+
+    Category toEntity(CreateCategoryRequestDTO request);
+
+    CategoryResponseDTO toResponse(Category category);
+
+    void updateEntityFromDTO(UpdateCategoryRequestDTO request, @MappingTarget Category category);
 }
