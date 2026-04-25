c24074f76917045a7eaa885862f2f706f70ba191
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:22:48 -0300

    refactor: add anotations in datetime

--- src/main/java/com/project/ecommerce/domain/payment/entity/Payment.java
+++ src/main/java/com/project/ecommerce/domain/payment/entity/Payment.java
@@ -1,9 +1,11 @@
-package com.project.ecomerce.domain.payment.entity;
+package com.project.ecommerce.domain.payment.entity;
 
-import com.project.ecomerce.domain.order.entity.Order;
-import com.project.ecomerce.shared.enums.PaymentStatus;
+import com.project.ecommerce.domain.order.entity.Order;
+import com.project.ecommerce.shared.enums.PaymentStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -11,47 +13,37 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "payments")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Payment {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Id private UUID id;
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false, unique = true)
     private Order order;
 
-    @Column(nullable = false)
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal amount;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private PaymentStatus status;
+    @Builder.Default
+    private PaymentStatus status = PaymentStatus.PENDING;
 
     @Column(name = "stripe_payment_intent_id")
     private String stripePaymentIntentId;
 
-    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-
-        if (this.status == null) {
-            this.status = PaymentStatus.PENDING;
-        }
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
