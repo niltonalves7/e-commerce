08920e9105653cd211afa8b85a05763485697342
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:02:02 -0300

    refactor: small adjustments

--- src/main/java/com/project/ecommerce/infra/security/service/CustomUserDetailsService.java
+++ src/main/java/com/project/ecommerce/infra/security/service/CustomUserDetailsService.java
@@ -1,32 +1,22 @@
-package com.project.ecomerce.infra.security.service;
+package com.project.ecommerce.infra.security.service;
 
-import com.project.ecomerce.domain.user.entity.User;
-import com.project.ecomerce.domain.user.repository.UserRepository;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.*;
+import com.project.ecommerce.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        User user = userRepository.findByEmail(email)
+        return userRepository.findByEmail(email)
+                .map(UserDetailsImpl::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                List.of(new SimpleGrantedAuthority(user.getRole().getRole()))
-        );
     }
 }
