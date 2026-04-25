36781abdcef5910ec7b1f705656a4c6a1bc70930
Nilton <nilttonalvez@gmail.com>
2026-04-25 20:03:50 -0300

    refactor: small adjustments

--- src/main/java/com/project/ecommerce/domain/cart/mapper/CartMapper.java
+++ src/main/java/com/project/ecommerce/domain/cart/mapper/CartMapper.java
@@ -1,4 +1,15 @@
 package com.project.ecommerce.domain.cart.mapper;
 
+import com.project.ecommerce.domain.cart.dto.response.CartItemResponseDTO;
+import com.project.ecommerce.domain.cart.entity.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface CartMapper {
+
+    @Mapping(target = "productId", source = "product.id")
+    @Mapping(target = "productName", source = "product.name")
+    @Mapping(target = "productImage", source = "product.imageUrl")
+    CartItemResponseDTO toItemResponse(CartItem item);
 }
