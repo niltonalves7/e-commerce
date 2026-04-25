c128f30783290220c3dc84bc41207d17f4639416
Nilton <nilttonalvez@gmail.com>
2026-04-25 19:30:40 -0300

    feat: implementing request for update category

--- src/main/java/com/project/ecommerce/domain/category/dto/request/UpdateCategoryRequestDTO.java
+++ src/main/java/com/project/ecommerce/domain/category/dto/request/UpdateCategoryRequestDTO.java
@@ -1,4 +1,14 @@
 package com.project.ecommerce.domain.category.dto.request;
 
-public class UpdateCategoryRequestDTO {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateCategoryRequestDTO(
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must be at most 100 characters")
+        String name,
+
+        @Size(max = 255, message = "Description must be at most 255 characters")
+        String description
+) {}
