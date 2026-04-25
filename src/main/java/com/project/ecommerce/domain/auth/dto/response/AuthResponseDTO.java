e707948dcdfa3854e76276966072ab0cccce070f
Nilton <nilttonalvez@gmail.com>
2026-04-25 20:08:35 -0300

    refactor: add token response

--- src/main/java/com/project/ecommerce/domain/auth/dto/response/AuthResponseDTO.java
+++ src/main/java/com/project/ecommerce/domain/auth/dto/response/AuthResponseDTO.java
@@ -1,4 +1,13 @@
 package com.project.ecommerce.domain.auth.dto.response;
 
-public class AuthResponseDTO {
+public record AuthResponseDTO(
+        String token,
+        String type,
+        String name,
+        String email,
+        String role
+) {
+    public AuthResponseDTO(String token, String name, String email, String role) {
+        this(token, "Bearer", name, email, role);
+    }
 }
