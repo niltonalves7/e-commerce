ca7be0f5fa40141ba0a3f83abe250785e68d84b8
Nilton <nilttonalvez@gmail.com>
2026-04-25 21:25:58 -0300

    feat: add swagger configuration

--- src/main/java/com/project/ecommerce/infra/config/SwaggerConfig.java
+++ src/main/java/com/project/ecommerce/infra/config/SwaggerConfig.java
@@ -1,4 +1,32 @@
 package com.project.ecommerce.infra.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-commerce API")
+                        .description("REST API for e-commerce platform with Stripe payment integration")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("Nilton Alves")
+                                .url("https://github.com/niltonalves7")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
