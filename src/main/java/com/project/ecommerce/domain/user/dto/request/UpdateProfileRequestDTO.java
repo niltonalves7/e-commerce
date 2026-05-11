0f299d459bd4a57fd9b924e8e70769696ed82bf5
Nilton <nilttonalvez@gmail.com>
2026-05-10 23:28:25 -0300

    feature: implementing request dto for update profile

--- src/main/java/com/project/ecommerce/domain/user/dto/request/UpdateProfileRequestDTO.java
+++ src/main/java/com/project/ecommerce/domain/user/dto/request/UpdateProfileRequestDTO.java
@@ -1,4 +1,17 @@
 package com.project.ecommerce.domain.user.dto.request;
 
-public record UpdateProfileRequestDTO() {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateProfileRequestDTO(
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 100, message = "Name must be at most 100 characters")
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        @Size(max = 150, message = "Email must be at most 150 characters")
+        String email
+) {}
